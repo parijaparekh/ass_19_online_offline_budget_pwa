bc2cb30f71a5fe8aa8519df82bbccfde098d3812
Ghibli <aran.t.ali@gmail.com>
2021-06-10 23:37:32 -0400

    service-worker is updated

--- public/service-worker.js
+++ public/service-worker.js
@@ -3,21 +3,12 @@ console.log("Hello from your service worker!");
 const FILES_TO_CACHE = [
   "/",
   "/index.html",
-  "/assets/css/style.css",
-  "/assets/js/loadPosts.js",
-  "/assets/images/Angular-icon.png",
-  "/assets/images/React-icon.png",
-  "/assets/images/Vue.js-icon.png",
-  "/manifest.webmanifest",
+  "/styles.css",
+  "/index.js",
+  "/db.js",
   "/favicon.ico",
-  "/assets/images/icons/icon-72x72.png",
-  "/assets/images/icons/icon-96x96.png",
-  "/assets/images/icons/icon-128x128.png",
-  "/assets/images/icons/icon-144x144.png",
-  "/assets/images/icons/icon-152x152.png",
-  "/assets/images/icons/icon-192x192.png",
-  "/assets/images/icons/icon-384x384.png",
-  "/assets/images/icons/icon-512x512.png",
+  "/icons/icon-192x192.png",
+  "/icons/icon-512x512.png",
 ];
 
 const CACHE_NAME = "static-cache-v2";
